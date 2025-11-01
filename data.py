2adfe59f9b8dfff32ffe3562c7a51b77a76502b5
anika3689 <raghava3@uci.edu>
2025-11-01 16:14:36 -0700

    added checks to prevent inf trapping

--- data.py
+++ data.py
@@ -4,10 +4,17 @@ from dataclasses import dataclass
 
 seed_url = "http://www.ics.uci.edu"
 
+RAW_RESPONSE_TEXT_LIMIT = 1 * 10 ** 6
+MIN_TEXT_THRESHOLD = 50
+MEANINGFUL_WORDCOUNT_RATIO = 0.35
+MAX_DEPTH = 50
+NUM_QUERY_PARAMS_THRESHOLD = 10
+
+
 @dataclass
 class CrawledData:
     word_freqs: ClassVar[Counter[str]] = Counter()
     links_to_words : ClassVar[Counter[str]] = Counter()
-    # using a separate set (visited) to also store urls of invalid urls
-    visited: ClassVar[set[str]] = set(seed_url)
+    # using a separate set (visited) to also store urls of invalid urls; values hold depth from seed
+    visited: ClassVar[dict] = {seed_url: 0}
     subdomains: ClassVar[Counter[str]] = Counter()
--- scraper.py
+++ scraper.py
@@ -1,23 +1,20 @@
 import re
 import os
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import urlparse, urlunparse, urljoin
 from bs4 import BeautifulSoup
 from tokenizer import tokenize
-from data import CrawledData
+from data import *
 
-RAW_RESPONSE_TEXT_LIMIT = 1 * 10 ** 6
-MIN_TEXT_THRESHOLD = 50
-MEANINGFUL_WORDCOUNT_RATIO = 0.5
 
 def scraper(url, resp, stopWords: dict[str]) -> list[ list[str], dict[str], int]:
     next_links, cur_page_words, total_word_count = [], {}, 0
     if resp.status > 199 and resp.status < 300 and len(resp.raw_response.content) < RAW_RESPONSE_TEXT_LIMIT:
         next_links, cur_page_words, total_word_count = extract_link_information(url, resp, stopWords)
-        skip_links = check_page_low_data()
+        skip_links = check_page_low_data(cur_page_words, total_word_count)
         if skip_links:
             next_links = []
         else:
-            next_links = [link for link in next_links if is_valid(link)]
+            next_links = [link for link in next_links if is_valid(link, url)]
     return next_links, cur_page_words, total_word_count
 
 def check_page_low_data(words_freqs, total_word_count):
@@ -29,7 +26,7 @@ def check_page_low_data(words_freqs, total_word_count):
         return True
     return False
 
-def extract_link_information(url, resp, stopWords, getWordCount=False) -> list[ list[str], dict[str], int]:
+def extract_link_information(url, resp, stopWords) -> list[ list[str], dict[str], int]:
     soup = BeautifulSoup(resp.raw_response.content)
     # What if the information successfully returned from site is not good
     tokenizer = tokenize(soup.stripped_strings, stopWords, countWords=True)
@@ -46,10 +43,14 @@ def extract_next_links(url, resp, soup):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    transformUrl = lambda url : url.get('href').split('#')[0]
-    return [transformUrl(link) for link in soup.find_all('a') if link]
+    links = []
+    for tag in soup.find_all('a', href=True):
+        href = tag['href'].split('#')[0]   # defragment
+        abs_url = urljoin(resp.url, href)  # resolve relative paths
+        links.append(abs_url)
+    return links
 
-def is_valid(url):
+def is_valid(url, parent_url=None):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
@@ -67,6 +68,8 @@ def is_valid(url):
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
             return False
+        if len(url) > 200 or url.count('&') > NUM_QUERY_PARAMS_THRESHOLD:
+            return False
         
         subdomain = parsed.hostname
         valid_domains = ('ics.uci.edu', 
@@ -77,10 +80,15 @@ def is_valid(url):
         if not subdomain or not any(subdomain.endswith(valid_dom) for valid_dom in valid_domains):
             return False
         CrawledData.subdomains[subdomain] += 1
+
         canonicalized_url = canonicalize(parsed)
+        parent_url = canonicalize(parent_url)
         if canonicalized_url in CrawledData.visited:
             return False
-        CrawledData.visited.add(canonicalized_url)
+        new_depth = CrawledData.visited[parent_url] + 1
+        if new_depth > MAX_DEPTH:
+            return False
+        CrawledData.visited[canonicalized_url] = new_depth
         return True
  
     except TypeError:
@@ -94,7 +102,7 @@ def canonicalize(parsed):
     scheme = parsed.scheme.lower()
     netloc = parsed.hostname.lower()
     # Normalize path
-    path = os.path.normpath(parsed.path)
+    path = "/" + os.path.normpath(parsed.path).lstrip("/")
     # Sort query params
     query = "&".join(sorted(parsed.query.split("&"))) if parsed.query else ""
     return urlunparse((scheme, netloc, path, "", query, ""))
