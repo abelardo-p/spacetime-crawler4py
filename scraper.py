47ede13cddec0cef43eaec5a7ecb34dd140f69d8
anika3689 <raghava3@uci.edu>
2025-11-01 13:51:02 -0700

    initial checks for low value pages

--- scraper.py
+++ scraper.py
@@ -5,15 +5,31 @@ from bs4 import BeautifulSoup
 from tokenizer import tokenize
 from data import CrawledData
 
+# 
+MIN_TEXT_THRESHOLD = 50
+MEANINGFUL_WORDCOUT_RATIO = 0.5
+
 def scraper(url, resp, stopWords: dict[str]) -> list[ list[str], dict[str], int]:
     next_links, cur_page_words, total_word_count = [], {}, 0
     if resp.status > 199 and resp.status < 300:
-        next_links, cur_page_words, totalWordCount = extract_link_information(url, resp, stopWords)
-        next_links = [link for link in next_links if is_valid(link)]
-        return next_links, cur_page_words, totalWordCount
-    return next_links, cur_page_words, totalWordCount
-    
-def extract_link_information(url, resp, stopWords, getWordCount=False) -> list[ list[str], dict[str : int] ]:
+        next_links, cur_page_words, total_word_count = extract_link_information(url, resp, stopWords)
+        skip_links = check_page_low_data()
+        if skip_links:
+            next_links = []
+        else:
+            next_links = [link for link in next_links if is_valid(link)]
+    return next_links, cur_page_words, total_word_count
+
+def check_page_low_data(words_freqs, total_word_count):
+    """Returns true if the links on this page should be skipped"""
+    if total_word_count < MIN_TEXT_THRESHOLD:
+        return True
+    meaningful_word_count = sum(words_freqs.values())
+    if meaningful_word_count / total_word_count < MEANINGFUL_WORDCOUT_RATIO:
+        return True
+    return False
+
+def extract_link_information(url, resp, stopWords, getWordCount=False) -> list[ list[str], dict[str], int]:
     soup = BeautifulSoup(resp.raw_response.content)
     tokenizer = tokenize(soup.stripped_strings, stopWords, countWords=True)
     links = extract_next_links(url, resp, soup)
